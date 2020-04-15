eee6e96b76b16851e300ed6e4f8dbc555b989bd9
MysticalGuest <1317148109@qq.com>
2020-04-15 15:32:03 +0800

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -2,3 +2,12 @@
  * api接口统一管理
  */
 import { get, post } from '@/request/http'
+
+export default {
+  postData(action, params){
+    return post(action, params)
+  },
+  getData(action, params){
+    return get(action, params)
+  }
+}
