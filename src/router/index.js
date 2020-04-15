47bc0824ac6693effaa809268b17f7391d730294
MysticalGuest <1317148109@qq.com>
2020-04-15 14:52:30 +0800

    update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import login from '../components/views/login.vue'
 
 Vue.use(Router)
 
@@ -10,6 +11,10 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },{
+      path: '/login',
+      name: 'login',
+      component: login
     }
   ]
 })
