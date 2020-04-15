13b3a552104514e91b537dc200895de0233ff3b4
MysticalGuest <1317148109@qq.com>
2020-04-15 13:17:03 +0800

    Axios cross-domain request

--- config/index.js
+++ config/index.js
@@ -6,11 +6,21 @@ const path = require('path')
 
 module.exports = {
   dev: {
+    env: require('./dev.env'),
 
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      // 跨域请求配置代理
+      '/api': {
+              target:'http://148.70.15.23:8000/', // 请求的第三方接口
+              changeOrigin:true, // 在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
+              pathRewrite:{  // 路径重写，
+                '^/api': ''  // 替换target中的请求地址，也就是说以后在请求http://148.70.15.23:8000/XXXXX这个地址的时候直接写成/api即可。
+              }
+            }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
@@ -20,7 +30,7 @@ module.exports = {
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
-    
+
     /**
      * Source Maps
      */
