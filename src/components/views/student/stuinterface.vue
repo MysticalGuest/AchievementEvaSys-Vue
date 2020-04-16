f52b0827d2ab0d482e39be957b047d68842cf2a2
MysticalGuest <1317148109@qq.com>
2020-04-16 13:31:02 +0800

    Create stuinterface.vue

---
+++ src/components/views/student/stuinterface.vue
@@ -0,0 +1,34 @@
+<template>
+  <header :name="name">87878</header>
+</template>
+
+<script>
+  import header from '../common/header.vue'
+  import '@/assets/css/style-header.css'
+
+  export default {
+  	data() {
+  		return {
+  			name: "肖梦杰"
+  		}
+  	},
+  	components: {
+  		header
+  	},
+  	methods: {
+  		// getInfo() {
+  		// 	TeacherApi.getInfo(3).then(res => {
+  		// 		this.$store.commit('modify', res.id)
+  		// 		this.name = res.name
+  		// 		this.id = res.id
+  		// 	})
+  		// }
+  	},
+  	created() {
+  		// this.getInfo()
+  	}
+  }
+</script>
+
+<style>
+</style>
