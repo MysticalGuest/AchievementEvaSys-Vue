461abe657c493b9c46c80e29a398a6d297a4cee8
MysticalGuest <1317148109@qq.com>
2020-04-17 09:58:54 +0800

    Create stuinfo.vue

---
+++ src/components/views/student/stuinfo.vue
@@ -0,0 +1,61 @@
+<template>
+  <!-- center -->
+  <div class="center">
+  	<div class="post__head">
+  		<a href="#" class="post__head-img">
+  			<img src="@/assets/img/user_black.png" alt="">
+  		</a>
+  		<div class="post__head-title">
+  			<h5><a href="#">肖梦杰</a></h5>
+  			<p>2017115146</p>
+  		</div>
+  	</div>
+
+  	<div class="post__wrap">
+  		<div class="post__company">
+  			<i class="icon ion-ios-briefcase"></i>
+  			<span>软件学院</span>
+  		</div>
+  	</div>
+
+  	<h2 class="post__title">基本信息</h2>
+
+  	<div class="post__options">
+  		<span>列表</span>
+  		<p>融入</p>
+  	</div>
+
+  	<div class="post__description">
+  		<p>房鼎益老师于第18周星期一第5-6节在3210上的计算机网络课程调课到由尹小燕老师在第18周星期五第5-6节3206上课</p>
+  		<a href="#">更多</a>
+  	</div>
+
+  	<div class="post__tags">
+  		<a href="#">HTML</a>
+  		<a href="#">CSS</a>
+  		<a href="#">PHP</a>
+  	</div>
+
+  	<div class="post__stats">
+  		<div>
+  			<a class="post__likes" href="#"><i class="icon ion-ios-heart"></i><span>15</span></a>
+  			<a class="post__comments" data-toggle="collapse" href="#collapse1" role="button" aria-expanded="false" aria-controls="collapse1"><i class="icon ion-ios-text"></i><span>0</span></a>
+  		</div>
+
+  		<div class="post__views">
+  			<i class="icon ion-ios-eye"></i><span>214</span>
+  		</div>
+  	</div>
+  </div>
+  <!-- end center -->
+</template>
+
+<script>
+  import '@/assets/css/style-center.css'
+  
+  export default {
+  }
+</script>
+
+<style>
+</style>
