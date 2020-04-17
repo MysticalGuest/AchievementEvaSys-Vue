7f879fe7a976222db52c8b0a753586d6ed58a94d
MysticalGuest <1317148109@qq.com>
2020-04-17 09:58:40 +0800

    Update header.vue

--- src/components/views/common/header.vue
+++ src/components/views/common/header.vue
@@ -1,64 +1,65 @@
 <template>
   <header class="header">
-  	<div class="container">
-  		<div class="row">
-  			<div class="col-7 col-md-9 col-lg-8 col-xl-9">
-  				<div class="header__content">
-  					<!-- header logo -->
-  					<a href="#" class="header__logo">
-  						<img src="@/assets/img/logo1.svg" alt="">
-  					</a>
-  					<!-- end header logo -->
+    <div class="header__content">
+      <!-- header logo -->
+      <a href="#" class="header__logo">
+        <img src="@/assets/img/logo1.svg" alt="">
+      </a>
+      <!-- end header logo -->
 
-  					<!-- header nav -->
-  					<ul class="header__nav">
-  						<li class="header__nav-item">
-  							<a href="#" class="header__nav-link">首页</a>
-  						</li>
-  						<li class="header__nav-item">
-  							<a href="#" class="header__nav-link">成绩</a>
-  						</li>
-  						<li class="header__nav-item">
-  							<a href="#" class="header__nav-link">页面</a>
-  						</li>
-  						<li class="header__nav-item">
-  							<a href="#" class="header__nav-link">页面</a>
-  						</li>
-  						<li class="header__nav-item">
-  							<a href="#" class="header__nav-link">页面</a>
-  						</li>
-  					</ul>
-  					<!-- end header nav -->
+      <!-- header nav -->
+      <ul class="header__nav">
+        <li class="header__nav-item">
+          <!-- <a href="interface" class="header__nav-link">首页</a> -->
+          <router-link to="interface" class="header__nav-link">首页</router-link>
+        </li>
+        <li class="header__nav-item">
+          <!-- <a href="#" class="header__nav-link">成绩</a> -->
+          <router-link :to="{path:route1}" class="header__nav-link">{{index1}}</router-link>
+        </li>
+        <li class="header__nav-item">
+          <!-- <a href="#" class="header__nav-link">页面</a> -->
+          <router-link :to="{path:route2}" class="header__nav-link">{{index2}}</router-link>
+        </li>
+        <li class="header__nav-item">
+          <router-link :to="{path:route3}" class="header__nav-link">{{index3}}</router-link>
+        </li>
+        <li class="header__nav-item">
+          <router-link to="/login" class="header__nav-link">{{index4}}</router-link>
+        </li>
+      </ul>
+      <!-- end header nav -->
 
-  					<!-- header search -->
-  					<form action="#" class="header__search">
-  						<input class="header__search-input" type="text" placeholder="Search...">
-  						<a class="header__search-button" type="button">
-  							<span class="glyphicon glyphicon-search"></span>
-  						</a>
-  					</form>
-  					<!-- end header search -->
-  				</div>
-  			</div>
-
-  			<div class="col-5 col-md-3 col-lg-4 col-xl-3">
-  				<div class="header__content header__content_end">
-  					<div class="header__profile">
-  						<a class="dropdown-toggle header__profile-btn" href="#" role="button" id="dropdownMenuProfile" aria-haspopup="true" aria-expanded="false">
-  							<img src="@/assets/img/user.png" alt="">
-  							<span>{{name}}</span>
-  						</a>
-  					</div>
-  				</div>
-  			</div>
-  		</div>
-  	</div>
+      <!-- header search -->
+      <form action="#" class="header__search">
+        <input class="header__search-input" type="text" placeholder="Search...">
+        <a class="header__search-button" type="button">
+          <!-- <span class="glyphicon glyphicon-search"></span> -->
+        </a>
+      </form>
+      <!-- end header search -->
+      <div class="header__profile">
+        <a class="dropdown-toggle header__profile-btn" href="#" role="button" id="dropdownMenuProfile" aria-haspopup="true" aria-expanded="false">
+          <img src="@/assets/img/user.png" alt="">
+          <span>{{name}}</span>
+        </a>
+      </div>
+    </div>
   </header>
 </template>
 
 <script>
+  import '@/assets/css/style-header.css'
+
   export default {
-  	props: ["name"],
+    props: ["name", "index1", "index2", "index3","index4", "route1", "route2", "route3"],
+    data() {
+    	return {
+        // getroute1:
+    	}
+    },
+
+
 
   	methods: {
   		handleOpen(key, keyPath) {
