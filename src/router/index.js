178f9c7954d0a07565d102cba195483f8388b3da
MysticalGuest <1317148109@qq.com>
2020-04-18 13:56:27 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,8 @@ import stuinterface from '../components/views/student/stuinterface.vue'
 import stuinfo from '../components/views/student/stuinfo.vue'
 import courseinfo from '../components/views/student/courseinfo.vue'
 
+import page404 from '../components/views/error/page404.vue'
+
 Vue.use(Router)
 
 export default new Router({
@@ -35,6 +37,8 @@ export default new Router({
           component:courseinfo
         }
       ]
-    }
+    },
+    // 404page
+    { path: '*', name: 'page404', component: page404 }
   ]
 })
