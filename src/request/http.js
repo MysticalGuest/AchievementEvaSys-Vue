9c8ef9646c5058b2ac5f929dfe01873639a21786
MysticalGuest <1317148109@qq.com>
2020-04-18 13:56:30 +0800

    Update http.js

--- src/request/http.js
+++ src/request/http.js
@@ -4,13 +4,16 @@ import Vuex from 'vuex'
 import Vue from 'vue'
 import axios from 'axios'
 import store from '@/store/index' //@ 等价于/src这个目录
+import router from '@/router'
 
 Vue.prototype.$axios = axios;
+Vue.prototype.$store = store;
 axios.defaults.baseURL = '/'  //关键代码
 // 设置默认的请求超时时间。超过了10s，告知用户当前请求超时，请刷新等。
 axios.defaults.timeout = 10000;
 // post请求头设置
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
+
 
 
 /*==============================
@@ -22,42 +25,60 @@ axios.interceptors.request.use(
         // 每次发送请求之前判断vuex中是否存在token
         // 如果存在，则统一在http请求的header都加上token，这样后台根据token判断你的登录情况
         // 即使本地存在token，也有可能token是过期的，所以在响应拦截器中要对返回状态进行判断
+        console.log("请求拦截器config"+config.headers.Authorization);
         const token = store.state.token;
+        console.log("store.state"+store.state);
+        console.log("token:"+token);
         token && (config.headers.Authorization = token);
+        // if (token) {  // 每次发送请求之前判断是否存在token，如果存在，则统一在http请求的header都加上token，不用每次请求都手动添加了
+        //             config.headers.Authorization = token;
+        //         }
         return config;
     },
     error => {
+        console.log("error:"+error);
         return Promise.error(error);
-})
+    }
+)
 
 // 响应拦截器
 axios.interceptors.response.use(
+    // 请求成功
     response => {
         // 如果返回的状态码为200，说明接口请求成功，可以正常拿到数据
         // 否则的话抛出错误
+        console.log("响应拦截器"+response.status);
         if (response.status === 200) {
-            return Promise.resolve(response);
+          console.log("响应拦截器"+response.status);
+          return Promise.resolve(response);
         } else {
-            return Promise.reject(response);
+          return Promise.reject(response);
         }
     },
     // 服务器状态码不是2开头的的情况
     // 这里可以跟你们的后台开发人员协商好统一的错误状态码
     // 然后根据返回的状态码进行一些操作，例如登录过期提示，错误提示等等
     // 下面列举几个常见的操作，其他需求可自行扩展
+    // 请求失败
     error => {
+      console.log("响应拦截器error::"+error);
         if (error.response.status) {
             switch (error.response.status) {
                 // 401: 未登录
                 // 未登录则跳转登录页面，并携带当前页面的路径
                 // 在登录成功后返回当前页面，这一步需要在登录页操作。
-                case 401:
+                case 401:{
+                  console.log("响应拦截器error::401");
+                  console.log("401::"+router);
+                  // router.push({ path: '/login'});
+                  console.log("401::"+router.currentRoute.fullPath);
                     router.replace({
                         path: '/login',
                         query: {
                             redirect: router.currentRoute.fullPath
                         }
                     });
+                }
                     break;
 
                 // 403 token过期
