8a8a601e6c1c56b5a5cab78d366470986cdf667f
MysticalGuest <1317148109@qq.com>
2020-04-20 23:19:49 +0800

    Update stuinterface.vue

--- src/components/views/student/stuinterface.vue
+++ src/components/views/student/stuinterface.vue
@@ -18,22 +18,19 @@
   import myheader from '../common/header.vue'
 
   import myside from '../common/side.vue'
-  import '@/assets/css/style-side.css'
-
-  // import mycenter from '../common/center.vue'
 
   export default {
   	data() {
   		return {
   			name: "",
-        route1: "courseinfo",
-        index1: "课程信息",
-        route2: "warning",
-        index2: "预警通知",
-        route3: "modifyinfor",
-        index3: "修改信息",
+        route1: "",
+        index1: "",
+        route2: "",
+        index2: "",
+        route3: "",
+        index3: "",
         index4: "退出系统",
-        stuclass: "软件工程"
+        stuclass: ""
   		}
   	},
   	components: {
@@ -41,33 +38,30 @@
       myside
   	},
   	methods: {
-  		// getInfo() {
-  		// 	TeacherApi.getInfo(3).then(res => {
-  		// 		this.$store.commit('modify', res.id)
-  		// 		this.name = res.name
-  		// 		this.id = res.id
-  		// 	})
-  		// }
   	},
   	created() {
-      // const sno = sessionStorage.getItem('sno');
       const token = sessionStorage.getItem('token');
       console.log("token:::"+token);
       const searchtoken = new URLSearchParams(token);
       let param = new URLSearchParams();
       param.append("sno", searchtoken.get("sno"));
-      // console.log("sno:::"+gettoken.sno);
       this.$api.postData('/stu/interface',param)
       .then(res => {
-        console.log("stuinterfaceres::::"+res.status);
-        console.log("stuinterfaceres::::"+res);
+        console.log("stuinterfaceres::::"+res.route1);
         this.name = res.name;
-        // this.name = ;
-
+        this.stuclass=res.stuclass;
+        this.route1 = res.route1;
+        this.index1 = res.index1;
+        this.route2 = res.route2;
+        this.index2 = res.index2;
+        this.route3 = res.route3;
+        this.index3 = res.index3;
       })
       .catch(err => {
         console.log(err);
       });
+
+
   	}
   }
 </script>
@@ -77,7 +71,7 @@
     position: relative;
     margin:0 auto;
     padding: 20px;
-    top: 50px;
+    top: 100px;
     border: 1px solid rgba(133,153,171,0.2);
     -webkit-box-shadow: 0 2px 26px 0 rgba(133,153,171,0.1);
     box-shadow: 0 2px 26px 0 rgba(133,153,171,0.1);
