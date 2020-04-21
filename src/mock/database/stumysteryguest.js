0da8bdfc8de6fbf4201ba047afb0d4d45af50782
MysticalGuest <1317148109@qq.com>
2020-04-21 15:48:32 +0800

    Update stumysteryguest.js

--- src/mock/database/stumysteryguest.js
+++ src/mock/database/stumysteryguest.js
@@ -2,7 +2,17 @@ let stuinfo = {
   sno: '2017115146',
   name: '肖梦杰',
   stuclass: "软件工程",
-  academy: "软件学院"
+  academy: "软件学院",
+  classnum:'4',
+  IDnum:"420621200001011234",
+  birth:"2000-01-01",
+  nation:"汉族",
+  political:"中国共产主义青年团团员",
+  origin:"湖北",
+  scategory:"普通本科生",
+  email:"vue@mail.com",
+  address:"湖北省襄阳市樊城区",
+  phone:"15172345643"
 }
 
 export default stuinfo
