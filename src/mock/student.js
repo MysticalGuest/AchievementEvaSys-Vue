af81c6ef86fcd16f51f10df884cc074e6a437809
MysticalGuest <1317148109@qq.com>
2020-04-23 21:33:27 +0800

    Update student.js

--- src/mock/student.js
+++ src/mock/student.js
@@ -12,19 +12,48 @@ import {mock} from './index.js'
 mock.onPost("/stu/interface").reply(config => {
   console.log("config  sno"+config.data);
   const data = new URLSearchParams(config.data);
+  console.log("sddsds"+Mysteryguest.stuinfo);
   // const thisstu = mergeJsonObject(Mysteryguest, stu);
   if(data.get("sno")=="111"){
-    return [200, mergeJsonObject(Mysteryguest, stu)];
+    return [200, mergeJsonObject(Mysteryguest.stuinfo, stu)];
   }
   return [401];
 });
 
 mock.onPost("/stu/info").reply(config => {
   console.log("config  sno"+config.data);
+  console.log("22222"+Mysteryguest.stuinfo);
   const data = new URLSearchParams(config.data);
-  // const thisstu = mergeJsonObject(Mysteryguest, stu);
+
   if(data.get("sno")=="111"){
-    return [200, Mysteryguest];
+    return [200, Mysteryguest.stuinfo];
+  }
+  return [401];
+});
+
+mock.onPost("/stu/courseinfo").reply(config => {
+  const data = new URLSearchParams(config.data);
+  if(data.get("sno")=="111"){
+    return [200, Mysteryguest.courseinfo];
+  }
+  return [401];
+});
+
+//获取学生所选课程大纲内容及其分值
+mock.onPost("/stu/courseDetailInfo").reply(config => {
+  const data = new URLSearchParams(config.data);
+  console.log("sdsdsd"+data.get("sno"));
+  if(data.get("sno")=="111"){
+    return [200, Mysteryguest.courseDetailData];
+  }
+  return [401];
+});
+
+//获取课程预警
+mock.onPost("/stu/warninfo").reply(config => {
+  const data = new URLSearchParams(config.data);
+  if(data.get("sno")=="111"){
+    return [200, Mysteryguest.warninfo];
   }
   return [401];
 });
