91a2892416c50b80c1e6e4d6cdf89b4a842eaafa
MysticalGuest <1317148109@qq.com>
2020-04-24 12:18:49 +0800

    Update student.js

--- src/mock/student.js
+++ src/mock/student.js
@@ -1,25 +1,18 @@
-// import axios from 'axios'
-// import MockAdapter from 'axios-mock-adapter'
+import {mock} from './index.js'
 
 import stu from './database/stu.js'
 import {mergeJsonObject} from './database/stu.js'
 import Mysteryguest from './database/stumysteryguest.js'
 
-import {mock} from './index.js'
-// const mock = new MockAdapter(axios);
-
-
 mock.onPost("/stu/interface").reply(config => {
-  console.log("config  sno"+config.data);
   const data = new URLSearchParams(config.data);
-  console.log("sddsds"+Mysteryguest.stuinfo);
-  // const thisstu = mergeJsonObject(Mysteryguest, stu);
   if(data.get("sno")=="111"){
     return [200, mergeJsonObject(Mysteryguest.stuinfo, stu)];
   }
   return [401];
 });
 
+// 学生端首页
 mock.onPost("/stu/info").reply(config => {
   console.log("config  sno"+config.data);
   console.log("22222"+Mysteryguest.stuinfo);
@@ -31,6 +24,7 @@ mock.onPost("/stu/info").reply(config => {
   return [401];
 });
 
+// 学生端学业信息
 mock.onPost("/stu/courseinfo").reply(config => {
   const data = new URLSearchParams(config.data);
   if(data.get("sno")=="111"){
@@ -39,7 +33,7 @@ mock.onPost("/stu/courseinfo").reply(config => {
   return [401];
 });
 
-//获取学生所选课程大纲内容及其分值
+// 获取学生所选课程大纲内容及其分值
 mock.onPost("/stu/courseDetailInfo").reply(config => {
   const data = new URLSearchParams(config.data);
   console.log("sdsdsd"+data.get("sno"));
@@ -49,7 +43,7 @@ mock.onPost("/stu/courseDetailInfo").reply(config => {
   return [401];
 });
 
-//获取课程预警
+// 获取课程预警
 mock.onPost("/stu/warninfo").reply(config => {
   const data = new URLSearchParams(config.data);
   if(data.get("sno")=="111"){
