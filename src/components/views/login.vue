338cc1acb150e332acb8c30240658499781ced03
MysticalGuest <1317148109@qq.com>
2020-04-24 12:18:39 +0800

    Update login.vue

--- src/components/views/login.vue
+++ src/components/views/login.vue
@@ -11,9 +11,9 @@
         </div>
         <div class="login_content">
           <div v-show="cur==0" class="Cbody_item">
-            <form v-on:submit.prevent="loginValidate">
-              <div class="form_item"><input type="text" ref="input_sno" name="sno" v-bind:class="isActive?'sno':'sno change'" placeholder="学号" autocomplete="off"></div>
-              <div class="form_item"><input type="password" ref="input_spassword" name="spassword" v-bind:class="isActive?'spassword':'spassword change'" placeholder="密码"></div>
+            <form v-on:submit.prevent="loginValidate(cur)">
+              <div class="form_item"><input type="text" ref="input_sno" name="sno" v-bind:class="stuisActive?'sno':'sno change'" placeholder="学号" autocomplete="off"></div>
+              <div class="form_item"><input type="password" ref="input_spassword" name="spassword" v-bind:class="stuisActive?'spassword':'spassword change'" placeholder="密码"></div>
               <div class="form_item">
                 <div class="fl"><input type="checkbox">记住密码</div>
                 <div class="fr"><a href="javascript:;">找回密码</a></div>
@@ -23,18 +23,18 @@
                 <input type="submit" name="" value="登录">
               </div>
             </form>
-
           </div>
           <div v-show="cur==1" class="Cbody_item">
-            <div class="form_item"><input type="text" name="tno" placeholder="教工号" autocomplete="off"></div>
-            <div class="form_item"><input type="password" name="tpassword" placeholder="密码"></div>
-            <div class="form_item">
-            </div>
-            <div class="clear"></div>
-            <div class="form_item">
-              <input type="submit" name="" value="登录">
-            </div>
-
+            <form v-on:submit.prevent="loginValidate(cur)">
+              <div class="form_item"><input type="text" ref="input_tno" name="tno" v-bind:class="teaisActive?'tno':'tno change'" placeholder="教工号" autocomplete="off"></div>
+              <div class="form_item"><input type="password" ref="input_tpassword" name="tpassword" v-bind:class="teaisActive?'spassword':'spassword change'" placeholder="密码"></div>
+              <div class="form_item">
+              </div>
+              <div class="clear"></div>
+              <div class="form_item">
+                <input type="submit" name="" value="登录">
+              </div>
+            </form>
           </div>
         </div>
       </div>
@@ -60,55 +60,89 @@
 				cur:0, //默认选中第一个tab
 				sno:"",
 				spassword:"",
-				// booldata:"",
-				isActive:true,
-        userToken:'',
+				stuisActive:true,
+        teaisActive:true,
 			}
 		},
 		created(){
 			// this.loginValidate("first");	// 在页面渲染完成即加载
 		},
 		methods:{
+      // flag即为this.cur值,用来识别入口Tab
 			loginValidate(flag){
-				if(flag!="first"){
+        console.log("flag:"+flag);
+        let param = new URLSearchParams();
+				if(flag==0){  // 学生登录入口
 					this.sno = this.$refs.input_sno.value;
 					this.spassword = this.$refs.input_spassword.value;
 					if(this.sno==""){
 						this.$refs.input_sno.placeholder="请输入学号！";
-						this.isActive = false;
+						this.stuisActive = false;
 						return false;
 					}
 					if(this.spassword==""){
 						this.$refs.input_spassword.placeholder="请输入密码！";
-						this.isActive = false;
+						this.stuisActive = false;
 						return false;
 					}
+          param.append("sno", this.sno);
+          param.append("spassword", this.spassword);
 				}
-				let param = new URLSearchParams();
-				param.append("sno", this.sno);
-				param.append("spassword", this.spassword);
+        else if(flag==1){   // 教工登录入口
+          this.tno = this.$refs.input_tno.value;
+          this.tpassword = this.$refs.input_tpassword.value;
+          if(this.tno==""){
+          	this.$refs.input_tno.placeholder="请输入教工号！";
+          	this.teaisActive = false;
+          	return false;
+          }
+          if(this.tpassword==""){
+          	this.$refs.input_tpassword.placeholder="请输入密码！";
+          	this.teaisActive = false;
+          	return false;
+          }
+          param.append("tno", this.tno);
+          param.append("tpassword", this.tpassword);
+        }
+
         this.$api.postData('/login',param)
 				.then(response => {
           console.log("response:"+response);
-					if(response==true){
-            console.log("43434"+response);
+					if(response=="student"){              // 学生
 						this.$router.push({ path: '/stu/interface'});
-						// const sessionSno = this.sno;
-						// sessionStorage.setItem('sno', sessionSno);
-            this.userToken = param;
-            console.log("userToken"+this.userToken);
             //将用户token保存到vuex中
-            // this.$api.commit('changelogin',{Authorization: this.userToken });
-            this.$store.commit('changeLogin',{token: this.userToken });
+            this.$store.commit('changeLogin',{token: param });
 					}
+          else if(response=="teacher"){         // 教师
+            this.$router.push({ path: '/tea/interface'});
+            this.$store.commit('changeLogin',{token: param });
+          }
+          else if(response=="coursemanager"){   // 课程负责人
+            this.$router.push({ path: '/com/interface'});
+            this.$store.commit('changeLogin',{token: param });
+          }
+          else if(response=="majormanager"){    // 专业负责人
+            this.$router.push({ path: '/mam/interface'});
+            this.$store.commit('changeLogin',{token: param });
+          }
+          else if(response=="tutor"){           // 导员
+            this.$router.push({ path: '/tor/interface'});
+            this.$store.commit('changeLogin',{token: param });
+          }
 					else if(response==false){
-						this.$refs.input_spassword.value = "";
-						this.$refs.input_spassword.placeholder='密码错误！';
-						this.isActive = false;
+            console.log("curafter:"+flag);
+            if(flag == 0){        // 学生入口
+              this.$refs.input_spassword.value = "";
+              this.$refs.input_spassword.placeholder='密码错误！';
+              this.stuisActive = false;
+            }
+						else if(flag == 1){  // 教工入口
+						  this.$refs.input_tpassword.value = "";
+						  this.$refs.input_tpassword.placeholder='密码错误！';
+						  this.teaisActive = false;
+						}
 					}
 				});
-        // let res = this.$axios.post('/login',"89");
-        // console.log(res);
 			}
 		},
 		// 组件内路由--进入组件时
