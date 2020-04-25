693496120476b9f06fc7536c671613b8c5335288
MysticalGuest <1317148109@qq.com>
2020-04-25 14:50:32 +0800

    Create teaIKun.js

---
+++ src/mock/database/teacher/teaIKun.js
@@ -0,0 +1,18 @@
+let teainfo = {
+  tno: '2017101',
+  name: '蔡徐坤',
+  department: "信息科学与技术学院",
+  academy: "信息科学与技术学院",
+  IDnum:"420621200001011235",
+  officenum:"302",
+  birth:"1990-01-02",
+  nation:"汉族",
+  political:"中国共产党党员",
+  origin:"湖南",
+  degree:"博士",
+  email:"vue@teamail.com",
+  address:"湖北省襄阳市襄城区",
+  phone:"18172345647"
+}
+
+export default teainfo
