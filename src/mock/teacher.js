04b08d03fa3e95f0cf75b7d19f75b17044225863
MysticalGuest <1317148109@qq.com>
2020-04-28 10:04:13 +0800

    Update teacher.js

--- src/mock/teacher.js
+++ src/mock/teacher.js
@@ -1,14 +1,15 @@
 import {mock} from './index.js'
 
+import {mergeJsonObject} from '@/mock/funlib.js'
+
 import tea from './database/teacher/tea.js'
-import {mergeJsonObject} from './database/teacher/tea.js'
 import iKun from './database/teacher/teaIKun.js'
 
 
 mock.onPost("/tea/interface").reply(config => {
   const data = new URLSearchParams(config.data);
   if(data.get("tno")=="222"){
-    return [200, mergeJsonObject(iKun, tea)];
+    return [200, mergeJsonObject(iKun.teainfo, tea)];
   }
   return [401];
 });
@@ -17,7 +18,25 @@ mock.onPost("/tea/interface").reply(config => {
 mock.onPost("/tea/info").reply(config => {
   const data = new URLSearchParams(config.data);
   if(data.get("tno")=="222"){
-    return [200, iKun];
+    return [200, iKun.teainfo];
   }
   return [401];
+});
+
+// 获取老师所教的课程
+mock.onPost("/tea/courseinfo").reply(config => {
+  const data = new URLSearchParams(config.data);
+  if(data.get("tno")=="222"){
+    return [200, iKun.courseData];
+  }
+  return [401];
+});
+
+mock.onPost("/tea/detailinfo").reply(config => {
+  const data = new URLSearchParams(config.data);
+  console.log("cno:"+data.get("cno"));
+  // if(data.get("tno")=="222"){
+    return [200, iKun.detailData];
+  // }
+  // return [401];
 });
