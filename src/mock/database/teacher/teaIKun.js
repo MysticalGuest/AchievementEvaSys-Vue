8cebd73c19b7e79798e8db9e2757775859a60c2f
MysticalGuest <1317148109@qq.com>
2020-04-28 10:03:54 +0800

    Update teaIKun.js

--- src/mock/database/teacher/teaIKun.js
+++ src/mock/database/teacher/teaIKun.js
@@ -15,4 +15,45 @@ let teainfo = {
   phone:"18172345647"
 }
 
-export default teainfo
+let courseData = [{
+    cno: "1",
+    cname: "数据结构",
+    nature: "必修课程",
+    ca_name: "张三",
+    tperiod: 36,
+    credit: 4,
+  },
+  {
+    cno: "2",
+    cname: "计算机网络",
+    nature: "必修课程",
+    ca_name: "张三",
+    tperiod: 54,
+    credit: 3.5,
+  },
+  {
+    cno: "3",
+    cname: "篮球",
+    nature: "选修课程",
+    ca_name: "张三",
+    tperiod: 72,
+    credit: 2,
+  },
+  {
+    cno: "4",
+    cname: "网页设计",
+    nature: "必修课程",
+    ca_name: "张三",
+    tperiod: 144,
+    credit: 1,
+  },
+]
+
+let detailData = [{
+  cname: "数据结构",//课程名
+  course_detail_name: "抹掉抹掉交付",//课程教学大纲
+  index_detail_id: "1",//指标点id
+  index_detail_content: "qwerrtyrtyr",//指标点的内容
+}]
+
+export default {teainfo, courseData, detailData}
