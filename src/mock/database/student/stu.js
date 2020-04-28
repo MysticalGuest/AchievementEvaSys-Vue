66f84acb622a6e5253df04437c2220aeb6c753b8
MysticalGuest <1317148109@qq.com>
2020-04-28 10:03:47 +0800

    Update stu.js

--- src/mock/database/student/stu.js
+++ src/mock/database/student/stu.js
@@ -1,6 +1,6 @@
 let stu = {
   role: "学生",
-  route1: "courseinfo",
+  route1: "academicinfo",
   index1: "学业信息",
   route2: "warninfo",
   index2: "预警通知",
@@ -10,15 +10,3 @@ let stu = {
 }
 
 export default stu
-
-// 用于合并学生基本信息
-export function mergeJsonObject(jsonbject1, jsonbject2) {
-  var resultJsonObject={};
-  for(var attr in jsonbject1){
-    resultJsonObject[attr]=jsonbject1[attr];
-  }
-  for(var attr in jsonbject2){
-    resultJsonObject[attr]=jsonbject2[attr];
-  }
-  return resultJsonObject;
-}
