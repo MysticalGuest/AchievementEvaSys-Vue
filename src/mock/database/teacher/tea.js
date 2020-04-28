adc9505ac30bf474c22b477fa397b123f0a953da
MysticalGuest <1317148109@qq.com>
2020-04-28 10:03:50 +0800

    Update tea.js

--- src/mock/database/teacher/tea.js
+++ src/mock/database/teacher/tea.js
@@ -1,24 +1,12 @@
 let tea = {
   role: "教师",
   route1: "courseinfo",
-  index1: "学业信息",
-  route2: "warninfo",
-  index2: "预警通知",
+  index1: "所授课程",
+  route2: "manageeva",
+  index2: "管理成绩",
   route3: "modifyinfo",
   index3: "修改信息",
   index4: "退出系统",
 }
 
 export default tea
-
-// 用于合并学生基本信息
-export function mergeJsonObject(jsonbject1, jsonbject2) {
-  var resultJsonObject={};
-  for(var attr in jsonbject1){
-    resultJsonObject[attr]=jsonbject1[attr];
-  }
-  for(var attr in jsonbject2){
-    resultJsonObject[attr]=jsonbject2[attr];
-  }
-  return resultJsonObject;
-}
