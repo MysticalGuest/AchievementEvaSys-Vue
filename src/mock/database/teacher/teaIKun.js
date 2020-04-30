dec5333c9851cb15f853f5c1d532b06f3470bce8
MysticalGuest <1317148109@qq.com>
2020-04-30 11:33:07 +0800

    Update teaIKun.js

--- src/mock/database/teacher/teaIKun.js
+++ src/mock/database/teacher/teaIKun.js
@@ -51,9 +51,52 @@ let courseData = [{
 
 let detailData = [{
   cname: "数据结构",//课程名
-  course_detail_name: "抹掉抹掉交付",//课程教学大纲
-  index_detail_id: "1",//指标点id
-  index_detail_content: "qwerrtyrtyr",//指标点的内容
+  course_detail_name: "目标 1. 掌握基软件测试的基本概念、基本理论、常用测试方法与测试技术。",//课程教学大纲
+  index_detail_id: "1-1",//指标点id
+  index_detail_content: "能够将数理知识、软件工程基础知识、软件专业知识及相关领域知识用于复杂软件问题的理解和表述。",//指标点的内容
 }]
 
-export default {teainfo, courseData, detailData}
+// 老师通知消息
+let inform = {
+  message : "课程审核通过！"
+}
+
+let stuData = [{
+  sno: "2017119075",
+  sname: '赵鹏程',
+  approval_status: '未审核'
+},{
+  sno: "2017119074",
+  sname: '赵小程',
+  approval_status: '未审核'
+},{
+  sno: "2017119073",
+  sname: '赵大程',
+  approval_status: '已审核'
+},{
+  sno: "2017119079",
+  sname: '赵晓程',
+  approval_status: '未审核'
+}]
+
+let detailState = [{
+  index_detail_id: '1-1',
+  appraisal_value: '0.76'
+},{
+  index_detail_id: '4-3',
+  appraisal_value: '0.98',
+  approval_status: '已审核',
+}]
+
+let index_options=[{
+  index_detail_id: '1-1',
+  label: "指标点1-1",
+  state: '未登记'
+},
+{
+  index_detail_id: '1-9',
+  label: "指标点1-9",
+  state: '已登记'
+}]
+
+export default {teainfo, courseData, detailData, inform, stuData, detailState, index_options}
