7464664b527d36bf21d38821ea168f7debb1a373
MysticalGuest <1317148109@qq.com>
2020-05-01 21:13:55 +0800

    Create mam.js

---
+++ src/mock/database/majormanager/mam.js
@@ -0,0 +1,12 @@
+let mam = {
+  role: "专业负责人",
+  route1: "formatgraduation",
+  index1: "格式化培养方案",
+  route2: "scoreanalysis",
+  index2: "成绩分析",
+  route3: "modifyinfo",
+  index3: "修改信息",
+  index4: "退出系统",
+}
+
+export default mam
