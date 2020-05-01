6e0789538a1880a195d3ffba2d111cd17668ccdd
MysticalGuest <1317148109@qq.com>
2020-05-01 21:14:08 +0800

    Create com.js

---
+++ src/mock/database/coursemanager/com.js
@@ -0,0 +1,12 @@
+let com = {
+  role: "课程负责人",
+  route1: "courselist",
+  index1: "负责课程",
+  route2: "reviewcourse",
+  index2: "审核课程",
+  route3: "modifyinfo",
+  index3: "修改信息",
+  index4: "退出系统",
+}
+
+export default com
