a9a864c851b9dc5cc3cfb1e2a18387814d61fd6e
MysticalGuest <1317148109@qq.com>
2020-05-01 21:14:25 +0800

    Create formatgraduation.vue

---
+++ src/components/views/majormanager/formatgraduation.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="center">
+    12345
+    <el-row>
+      <drag-verify
+      ref="dragVerify"
+      :isPassing.sync="isPassing2"
+      text="请按住滑块拖动"
+      successText="验证通过"
+      handlerIcon="el-icon-d-arrow-right"
+      successIcon="el-icon-circle-check"
+      >
+    </drag-verify>
+    </el-row>
+    
+  </div>
+</template>
+
+<script>
+  // 基本滑块验证组件
+  import dragVerify from "@/components/views/common/dragVerify";
+  export default {
+    components:{
+      dragVerify
+    }
+  }
+</script>
+
+<style>
+
+</style>
