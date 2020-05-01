073104364aa1c0760c1137af3e9d8b5895b87346
MysticalGuest <1317148109@qq.com>
2020-05-01 21:14:29 +0800

    Create reviewcourse.vue

---
+++ src/components/views/coursemanager/reviewcourse.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="center">
+    <el-table :data="reviewData.slice((currentPage-1) * pagesize, currentPage * pagesize)" style="width: 100%">
+      <el-table-column prop="cno" label="课程id" sortable column-key="courseId">
+      </el-table-column>
+      <el-table-column prop="cname" label="课程名">
+      </el-table-column>
+      <el-table-column fixed="right" label="操作">
+        <template slot-scope="scope">
+          <el-button @click="showStu(scope.row)" type="warning">审核</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <p></p>
+    <div style="margin: 0 auto;margin-top: 30px;">
+      <el-pagination background layout="prev, pager, next" :total="total" @current-change="current_change">
+      </el-pagination>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        reviewData:[]
+      }
+    },
+    methods:{
+      showStu(row){
+
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
