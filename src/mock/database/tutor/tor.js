49981f32628ba2becc5e01fad65d73d3313c26ca
MysticalGuest <1317148109@qq.com>
2020-05-07 11:33:19 +0800

    Create tor.js

---
+++ src/mock/database/tutor/tor.js
@@ -0,0 +1,12 @@
+let tor = {
+  role: "辅导员",
+  route1: "stulist",
+  index1: "学生信息",
+  route2: "analysis",
+  index2: "分析数据",
+  route3: "modifyinfo",
+  index3: "修改信息",
+  index4: "退出系统",
+}
+
+export default tor
