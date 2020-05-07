22407dc16bbe07445eaaf945d03116f6895faf4a
MysticalGuest <1317148109@qq.com>
2020-05-07 11:33:28 +0800

    Update majormanager.js

--- src/mock/majormanager.js
+++ src/mock/majormanager.js
@@ -13,11 +13,29 @@ mock.onPost("/mam/interface").reply(config => {
   return [401];
 });
 
-// 教师端首页
+// 专业负责人端首页
 mock.onPost("/mam/info").reply(config => {
   const data = new URLSearchParams(config.data);
   if(data.get("tno")=="444"){
     return [200, Joker.maminfo];
   }
+  return [401];
+});
+
+// 获取所有指标点
+mock.onPost("/mam/getAllIndexDetail").reply(config => {
+  // const data = new URLSearchParams(config.data);
+  // if(data.get("tno")=="444"){
+    return [200, Joker.allIndexDetail];
+  // }
+  return [401];
+});
+
+// 获取整个年级个人评价值平均分
+mock.onPost("/mam/getAvg").reply(config => {
+  // const data = new URLSearchParams(config.data);
+  // if(data.get("tno")=="444"){
+    return [200, Joker.score];
+  // }
   return [401];
 });
