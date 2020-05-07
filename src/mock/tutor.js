e401e4bdb74c5082423eeaf7ce1639b2d38ef90c
MysticalGuest <1317148109@qq.com>
2020-05-07 11:33:33 +0800

    Create tutor.js

---
+++ src/mock/tutor.js
@@ -0,0 +1,23 @@
+import {mock} from './index.js'
+
+import {mergeJsonObject} from '@/mock/funlib.js'
+
+import tor from './database/tutor/tor.js'
+import GEM from './database/tutor/torGEM.js'
+
+mock.onPost("/tor/interface").reply(config => {
+  const data = new URLSearchParams(config.data);
+  if(data.get("tno")=="555"){
+    return [200, mergeJsonObject(GEM.torinfo, tor)];
+  }
+  return [401];
+});
+
+// 教师端首页
+mock.onPost("/tor/info").reply(config => {
+  const data = new URLSearchParams(config.data);
+  if(data.get("tno")=="555"){
+    return [200, GEM.torinfo];
+  }
+  return [401];
+});
