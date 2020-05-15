d983665d9306dc26ab295b172778b61138a94ed9
MysticalGuest <1317148109@qq.com>
2020-05-15 09:41:52 +0800

    Update majormanager.js

--- src/mock/majormanager.js
+++ src/mock/majormanager.js
@@ -39,3 +39,65 @@ mock.onPost("/mam/getAvg").reply(config => {
   // }
   return [401];
 });
+
+// 格式化培养方案第一步,获得表格毕业要求数据
+mock.onPost("/mam/getIndex").reply(config => {
+  // const data = new URLSearchParams(config.data);
+  // if(data.get("tno")=="444"){
+    return [200, Joker.indexData];
+  // }
+  return [401];
+});
+
+// 格式化培养方案第二步,选择一个毕业要求,获得表格指标点数据
+mock.onPost("/mam/getIndexDetail").reply(config => {
+  const data = new URLSearchParams(config.data);
+  console.log("index_id"+data.get("index_id"));
+  if(data.get("index_id")=="1"){
+    return [200, Joker.indexDetailOfIndex];
+  }
+  // return [401];
+});
+
+// 添加毕业要求
+mock.onPost("/mam/submitIndex").reply(config => {
+  const data = new URLSearchParams(config.data);
+  console.log("newIndexName"+data.get("newIndexName"));
+  console.log("newIndexContent"+data.get("newIndexContent"));
+  // if(data.get("index_id")=="1"){
+    return [200, Joker.addIndexResult];
+  // }
+  // return [401];
+});
+
+// 移除毕业要求
+mock.onPost("/mam/removeIndex").reply(config => {
+  const data = new URLSearchParams(config.data);
+  console.log("index_id"+data.get("index_id"));
+  // if(data.get("index_id")=="1"){
+    return [200, Joker.removeIndexResult];
+  // }
+  // return [401];
+});
+
+// 添加指标点
+mock.onPost("/mam/submitIndexDetail").reply(config => {
+  const data = new URLSearchParams(config.data);
+  console.log("indexdetailvalue"+data.get("indexdetailvalue"));
+  console.log("newIndexDetailContent"+data.get("newIndexDetailContent"));
+  // if(data.get("index_id")=="1"){
+    return [200, Joker.addIndexResult];
+  // }
+  // return [401];
+});
+
+// 移除指标点
+mock.onPost("/mam/removeIndexDetail").reply(config => {
+  const data = new URLSearchParams(config.data);
+  console.log("indexdetailvalue"+data.get("indexdetailvalue"));
+  console.log("index_detail_id"+data.get("index_detail_id"));
+  // if(data.get("index_id")=="1"){
+    return [200, Joker.removeIndexDetailResult];
+  // }
+  // return [401];
+});
