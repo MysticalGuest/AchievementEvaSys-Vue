c1767c30be78a580cb41474a71ed0d260218cb39
MysticalGuest <1317148109@qq.com>
2020-06-09 22:42:30 +0800

    Update stulist.vue

--- src/components/views/tutor/stulist.vue
+++ src/components/views/tutor/stulist.vue
@@ -1,13 +1,43 @@
 <template>
   <div class="center">
-    1212ddd
+    <el-table ref="filterTable" :data="stuData.slice((currentPage-1) * pagesize, currentPage * pagesize)" style="width: 100%" border>
+      <el-table-column prop="sno" label="学号" sortable column-key="sno" align="center"></el-table-column>
+      <el-table-column prop="sname" label="姓名" align="center"></el-table-column>
+      <el-table-column prop="class" label="班级" align="center"></el-table-column>
+    </el-table>
+    <el-pagination background layout="prev, pager, next" :total="total" @current-change="current_change">
+    </el-pagination>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+  export default {
+    data(){
+      return {
+        total: 0,
+        pagesize: 4,
+        currentPage: 1,
+        stuData: [],
+      }
+    },
+    created() {
+      // 获取token;数据sno=111&spassword=333
+      const token = sessionStorage.getItem('token');
+      // 构造URL 的查询字符串对象
+      const searchtoken = new URLSearchParams(token);
+      let param = new URLSearchParams();
+      // 获取tno数据
+      param.append("tno", searchtoken.get("tno"));
+      this.$api.postData('/tor/stulist', param)
+      .then(res => {
+        this.stuData = res;
+        this.total = res.length;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    }
+  }
 </script>
 
 <style>
