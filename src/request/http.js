e19be7743629fc6ae007224487a8046a1eb9ca22
MysticalGuest <1317148109@qq.com>
2020-06-09 22:42:44 +0800

    Update http.js

--- src/request/http.js
+++ src/request/http.js
@@ -136,14 +136,14 @@ axios.interceptors.response.use(
 /*==============================
 		封装get方法和post方法
 	==============================*/
-// const Domain = "http://148.70.15.23:8000";  // 定义根域名
+// const Domain = "http://255.255.255.0:8000";  // 定义根域名
 const Domain = "";
 /**
  * get方法，对应get请求
  * @param {String} url [请求的url地址]
  * @param {Object} params [请求时携带的参数]
  */
-export function get(sction, params){
+export function get(action, params){
     return new Promise((resolve, reject) =>{
         // url 判断是测试环境 就要拿 测试环境的域名， 正式环境的就要用 正式域名
         let url = Domain + action;
